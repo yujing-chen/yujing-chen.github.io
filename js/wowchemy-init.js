fc238de3896d0f72c789349c1040637940fc5a49
yujing-chen <chenyujing@outlook.com>
2020-12-30 00:08:54 -0500

    t

--- js/wowchemy-init.js
+++ js/wowchemy-init.js
@@ -1,5 +1,5 @@
 (() => {
-  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20201228231801-219b05cbd2c4/assets/js/wowchemy-theming.js
+  // ns-hugo:/var/folders/1r/nqlry4pd77d_x4tf_7gq5_xw0000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20201228231801-219b05cbd2c4/assets/js/wowchemy-theming.js
   var body = document.body;
   function getThemeMode() {
     return parseInt(localStorage.getItem("wcTheme") || 2);
